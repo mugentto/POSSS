0fd3c632732c9a36c49c95c0ea5e5c2d7f44885e
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2024-08-05 11:49:07 +0700

    feat: add admin dashboard template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/',[AdminController::class,"index"]);
+
+
