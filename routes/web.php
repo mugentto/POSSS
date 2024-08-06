918884141e28ee0d452c8abd225981b55184560b
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2024-08-06 21:43:09 +0700

    clean dashboard admin template

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
+
+Route::get('/',[AdminController::class,"index"]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
